c94200def2ab8e8499f95d420f45b374ce0d5899
tonymans33 <tonymansour66@yahoo.com>
2021-09-27 02:42:03 +0200

    Fixing ejs bug

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+var path = require('path');
 const apiRouter = require('./routes/api')
 
 var app = express()
+
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use("/api", apiRouter);
--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,9 +1,9 @@
-
+const { v4: uuidV4 } = require('uuid')
 
 exports.generateUuid = (req, res) => {
-    res.redirect(`/${uuidv4()}`)
+    res.redirect(`/api/rooms/${uuidV4()}`);
 }
 
 exports.getChat = (req, res) => {
-    res.render('../views/room', { roomId: req.params.room })
+    res.render('room', { roomId: req.params.room })
 }
